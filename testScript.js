3e6d12cb10ad428aede0aac0858b250155323a28
AshifAli007 <theashifkhan@gmail.com>
2022-12-20 08:28:56 +0530

    divide function added

--- testScript.js
+++ testScript.js
@@ -4,6 +4,10 @@ const add = (a, b) => {
 const multiply = (a, b) => {
   return a * b;
 };
+const divide = (a, b) => {
+  return a / b;
+};
 
 console.log({ sum: add(9, 19) });
 console.log({ multiplication: multiply(9, 19) });
+console.log({ division: divide(9, 19) });
