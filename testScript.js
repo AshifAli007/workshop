2e9c7c8456f1472075e01c37acc4b6258ad4e9da
AshifAli007 <theashifkhan@gmail.com>
2022-12-20 08:27:05 +0530

    multiply function added

--- testScript.js
+++ testScript.js
@@ -1,5 +1,9 @@
 const add = (a, b) => {
   return a + b;
 };
+const multiply = (a, b) => {
+  return a * b;
+};
 
 console.log({ sum: add(9, 19) });
+console.log({ multiplication: multiply(9, 19) });
